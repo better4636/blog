3035da6b69df89f7b8adf9fa7e9d4ecf537659e1
better4636 <better4636@naver.com>
2021-09-20 21:28:50 +0900

    :building_construction: 개발환경 마련 (lint, husky)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+};
